c89330d3a349d53820bbdcdfe3db3c7087098932
hcustovic1 <haris.custovic@coachhub.io>
2021-06-20 20:44:55 +0200

    chore: test files updated

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,20 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    const { container } = render(<App />);
+    expect(container).toMatchInlineSnapshot(`
+        <div>
+          <div
+            class="sc-bdnxRM iOCJYU"
+          >
+            <header
+              class="sc-gtsrHT jfwppf"
+            >
+              <div>
+                Sarma ovo je tvoj dashboard
+              </div>
+            </header>
+          </div>
+        </div>
+    `);
 });
