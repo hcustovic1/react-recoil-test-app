941c9612e42789aab56c1965ee1e165c21fac13c
hcustovic1 <haris.custovic@coachhub.io>
2021-06-20 18:49:57 +0200

    chore: package fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Start from './pages/Start';
-import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import { mainTheme } from './styles/theme';
+import { StyledApp, StyledAppBody } from './styles/components';
 
 function App() {
-    const StyledApp = styled.div`
-        text-align: center;
-    `;
-
-    const StyledAppBody = styled.header`
-        background-color: #282c34;
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        font-size: calc(10px + 2vmin);
-        color: white;
-    `;
-
     return (
         <Router>
-            <StyledApp>
-                <StyledAppBody>
-                    <Route exact path="/start">
-                        <Start />
-                    </Route>
-                    <Route exact path="/">
-                        <Dashboard />
-                    </Route>
-                </StyledAppBody>
-                <Switch></Switch>
-            </StyledApp>
+            <ThemeProvider theme={mainTheme}>
+                <StyledApp>
+                    <StyledAppBody>
+                        <Route exact path="/start">
+                            <Start />
+                        </Route>
+                        <Route exact path="/">
+                            <Dashboard />
+                        </Route>
+                    </StyledAppBody>
+                    <Switch></Switch>
+                </StyledApp>
+            </ThemeProvider>
         </Router>
     );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const mainTheme = {
+    colors: {
+        queenPink: '#EACBD2',
+        pastelPink: '#DFAEB4',
+        kobi: '#DD9AC2',
+        operaMauve: '#B486AB',
+        chineseViolet: '#82667F',
+    },
+};
